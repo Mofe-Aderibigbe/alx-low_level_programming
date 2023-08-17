7e583338cc4f3361683ade30cc9ddabfb37bd5ee
Mofe-Aderibigbe <mofeaderibigbe@gmail.com>
2023-08-16 19:59:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet .
+ * print_alphabet - check the code for ALX School students.
  *
  * Return is Always 0.
  */
 void print_alphabet(void)
 {
 	char alp;
+
 	for (alp = 'a'; alp <= 'z'; alp++)
 	_putchar(alp);
 	_putchar('\n');

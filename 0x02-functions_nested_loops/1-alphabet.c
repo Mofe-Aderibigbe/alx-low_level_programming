747988abd4236c3c83794577e0b6c21fe453ddbd
Mofe-Aderibigbe <mofeaderibigbe@gmail.com>
2023-08-16 19:53:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet .
+ *
+ * Return is Always 0.
+ */
+void print_alphabet(void)
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++)
+	_putchar(alp);
+	_putchar('\n');
+}
